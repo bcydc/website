77110c0b30a4745fc4762c1f4ca58505f6b03f1b
Kevin Thomas <kevin.jt2007@gmail.com>
2023-11-11 15:53:25 -0800

    fix: grid

--- src/app/components/LandingBackground.tsx
+++ src/app/components/LandingBackground.tsx
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
+
 export default function LandingBackground() {
   return (
-    <div className="w-full">
+    <Fragment>
       <div className="bg-gradient absolute -top-1/2 h-screen w-full" />
-      <div className="bg-grid absolute left-0 top-0 h-screen w-full opacity-[0.04]">
+      <div className="absolute left-0 top-0 flex h-screen w-screen overflow-x-hidden opacity-[0.04]">
+        <div className="flex gap-20">
+          {Array(20)
+            .fill(0)
+            .map((x, i) => (
+              <div key={i} className="h-screen w-[1px] bg-white" />
+            ))}
+        </div>
         <div className="absolute left-0 top-0 flex w-full flex-col gap-20">
           {Array(10)
             .fill(0)
@@ -10,14 +19,7 @@ export default function LandingBackground() {
               <div key={i} className="h-[1px] w-full bg-white" />
             ))}
         </div>
-        <div className="absolute left-0 top-0 flex w-full flex-row gap-20 overflow-x-hidden">
-          {Array(20)
-            .fill(0)
-            .map((x, i) => (
-              <div key={i} className="h-screen w-[1px] bg-white" />
-            ))}
-        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
