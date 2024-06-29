6303015a1ca6d21b416ebf4178db3522d4f5e83a
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 16:19:49 -0700

    feat: modified navbar and fixed movile

--- src/ui/components/MobileNavbar.tsx
+++ src/ui/components/MobileNavbar.tsx
@@ -2,78 +2,24 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 
 export default function MobileNavbar({
   links,
 }: {
   links: { label: string; href: string }[];
 }) {
-  const [sidebar, setSidebar] = useState(false);
-
   return (
-    <div className="flex md:hidden items-center w-full justify-between py-8 px-4">
-      <AnimatePresence>
-        {sidebar && (
-          <motion.div
-            className="absolute top-0 left-0 z-10 w-screen h-screen bg-black bg-opacity-80"
-            onClick={() => setSidebar(false)}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.1 }}
-          >
-            <motion.div
-              className="flex flex-col p-8 justify-between gap-6 w-5/6 h-full bg-g-800 border-r-2 border-g-700"
-              onClick={(e) => e.stopPropagation()}
-              initial={{ x: "-100%" }}
-              animate={{ x: 0 }}
-              exit={{ x: "-100%" }}
-              transition={{ duration: 0.2 }}
-            >
-              <div className="gap-6 flex flex-col">
-                <div className="relative w-12 h-12">
-                  <Image src="/icons/logo.svg" alt="Logo" fill={true} />
-                </div>
-                <div className="flex flex-col gap-3">
-                  {links.map((link, i) => (
-                    <a href={link.href} key={i}>
-                      <p className="text-3xl text-white">{link.label}</p>
-                    </a>
-                  ))}
-                </div>
-              </div>
-              <a
-                href="https://discord.bcydc.ca/"
-                target="_blank"
-                className="flex w-full items-center rounded-lg justify-center bg-g-700 py-4 gap-2.5"
-              >
-                <div className="relative w-5 h-5">
-                  <Image
-                    alt="Discord Logo"
-                    src="/icons/discord.svg"
-                    fill={true}
-                  />
-                </div>
-                <p className="text-xl text-white font-medium">
-                  Join our Discord
-                </p>
-              </a>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+    <div className="flex w-full items-center justify-between py-8 md:hidden">
       <Link href="/" className="flex items-center gap-3">
-        <div className="relative w-10 h-10">
+        <div className="relative h-10 w-10">
           <Image src="/icons/logo.svg" alt="Logo" fill={true} />
         </div>
-        <h1 className="text-lg text-white max-w-[200px] leading-tight font-semibold">
+        <h1 className="max-w-[200px] text-lg font-semibold leading-tight text-white">
           British Columbia Youth Developer Collective
         </h1>
       </Link>
-      <div className="w-8 h-8 relative" onClick={() => setSidebar(true)}>
-        <Image src="/icons/bars.svg" fill={true} alt="Hamburger" />
+      <div className="relative h-8 w-8">
+        <Image alt="Discord Logo" src="/icons/discord.svg" fill={true} />
       </div>
     </div>
   );
--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -8,8 +8,6 @@ import { usePathname } from "next/navigation";
 import MobileNavbar from "./MobileNavbar";
 
 export default function Navbar() {
-  const path = usePathname();
-
   const links = [
     {
       label: "Mentors",
@@ -20,7 +18,7 @@ export default function Navbar() {
       href: "https://bcydc.notion.site/09cfba7470824077b94d2d98d50708b3",
     },
     {
-      label: "Events",
+      label: "Programs",
       href: "https://bcydc.notion.site/0577cb29f6d54cf0a345c58eb9b127fd",
     },
     {
@@ -33,24 +31,33 @@ export default function Navbar() {
     <Fragment>
       <MobileNavbar links={links} />
       <div
-        className={`hidden md:flex z-10 ${
-          path !== "/" && "bg-g-800"
-        } items-center w-full py-10 justify-between px-4 md:px-8`}
+        className={`z-10 hidden items-center justify-between py-10 md:flex md:w-[1024px] md:max-w-[1024px]`}
       >
         <Link href="/" className="flex items-center gap-3">
-          <div className="relative w-6 xl:w-8 h-6 xl:h-8">
+          <div className="relative h-6 w-6">
             <Image src="/icons/logo.svg" alt="Logo" fill={true} />
           </div>
-          <h1 className="text-xl xl:text-3xl text-white font-medium">
+          <h1 className="text-2xl font-medium text-white">
             British Columbia Youth Developer Collective
           </h1>
         </Link>
         <div className="flex items-center gap-3 xl:gap-6">
-          {links.map((link, i) => (
+          <a
+            href="https://discord.bcydc.ca/"
+            target="_blank"
+            className="relative flex select-none items-center gap-2 overflow-hidden rounded-lg bg-gradient-to-r from-g-700 to-g-500 px-5 py-2"
+          >
+            <div className="absolute left-0 top-0 h-full w-full bg-black bg-opacity-0 transition duration-300 hover:bg-opacity-10" />
+            <div className="relative h-4 w-4">
+              <Image alt="Discord Logo" src="/icons/discord.svg" fill={true} />
+            </div>
+            <p className="font-medium text-white">Join</p>
+          </a>
+          {/* {links.map((link, i) => (
             <a href={link.href} key={i} target="_blank">
-              <h1 className="text-lg xl:text-2xl text-white">{link.label}</h1>
+              <h1 className="text-lg text-white">{link.label}</h1>
             </a>
-          ))}
+          ))} */}
         </div>
       </div>
     </Fragment>
