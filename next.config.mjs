375c865da4a61d8fa53871cce5ba83af19c24c22
Vuk <vuk.tacic@gmail.com>
2025-07-06 12:03:07 -0700

    next conf upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { dev }) => {
+        if (dev) {
+            config.watchOptions = {
+                poll: true, // Fix for hot reloading
+            }
+
+            return config;
+        }
+
+        return config;
+    }
+};
 
 export default nextConfig;
