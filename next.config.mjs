905b240a176693f94297e15f0697ed056a6aa0a4
Vuk <vuk.tacic@gmail.com>
2025-07-06 01:12:41 -0700

    Reset config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack: (config, { dev }) => { 
-        if (dev) {
-            config.watchOptions = {
-                poll: true, // Fix for hot reloading
-            }
+    // webpack: (config, { dev }) => { 
+    //     if (dev) {
+    //         config.watchOptions = {
+    //             poll: true, // Fix for hot reloading
+    //         }
 
-            return config;
-        }
+    //         return config;
+    //     }
 
-        return config;
-    }
+    //     return config;
+    // }
 };
 
 export default nextConfig;
