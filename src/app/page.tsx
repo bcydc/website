3102c8c11e2b44752bc949bd357f7f96e69fb4e0
Vuk <vuk.tacic@gmail.com>
2025-07-31 11:41:38 -0700

    Add google tag to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Team from "./components/Team";
 import Landing from "./components/Landing";
 import Programs from "./components/Programs";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Landing />
       <Programs />
       <Team />
+      <GoogleTagManager gtmId="G-JD2FNNVXTR"/>
     </div>
   );
 }
